ca70e64aba5e793beaf555e2ccdfc12bd0c3cf91
J Neo <jo.neoyj@gmail.com>
2023-03-12 21:14:28 +0800

    Added Security configuration key cloak to API Gateway. Added authentication to the other services

---
+++ ecommerce-microservices/discovery-server/src/main/java/com/ecommerce/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.ecommerce.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  @Value("${eureka.username}")
+  private String username;
+  @Value("${eureka.password}")
+  private String password;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf().disable()
+            .authorizeRequests(authorize -> authorize
+                    .anyRequest().authenticated()
+            )
+            .httpBasic();
+
+    return http.build();
+  }
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+            .username(username)
+            .password(password)
+            .roles("USER")
+            .build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+}
+
+
