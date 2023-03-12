1ff264b13ae7c20b4631d12e7138596c98f73daf
J Neo <jo.neoyj@gmail.com>
2023-03-12 17:45:33 +0800

    Included client instance loadbalancing

--- ecommerce-microservices/order-service/src/main/java/com/ecommerce/orderservice/config/WebClientConfig.java
+++ ecommerce-microservices/order-service/src/main/java/com/ecommerce/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.ecommerce.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
   @Bean
-  public WebClient webClient() {
-    return WebClient.builder().build();
+  @LoadBalanced
+  public WebClient.Builder webClientBuilder() {
+    return WebClient.builder(); // automatically create a client side instance
+    // wont get confused when finding more than one isntance
   }
 }
--- ecommerce-microservices/order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
+++ ecommerce-microservices/order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
   private final OrderRepository orderRepository;
-  private final WebClient webClient;
+  private final WebClient.Builder webClientBuilder;
 
   public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
     Order order = new Order();
@@ -40,8 +40,8 @@ public class OrderService {
             .toList();
 
     // call inventory service, and place order if product is in stock
-    InventoryResponse[] inventoryResponseArray = webClient.get()
-            .uri("http://localhost:8082/api/inventory",
+    InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+            .uri("http://inventory-service/api/inventory",
                     uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
             .retrieve()
             .bodyToMono(InventoryResponse[].class)// todo: check the mono class
